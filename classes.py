9927c3327f2ed615d83a46f4d130e485a33c689f
user <iiiskhakov1990@gmail.com>
2023-04-17 01:12:18 +0300

    Заполнение файла main

--- classes.py
+++ classes.py
@@ -1,20 +1,14 @@
 from abc import ABC, abstractmethod
 from functions import *
+from global_variables import *
 
 import requests
 import json
 
 
-PRICE_EUR = 85
-PRICE_USD = 80
-PAGE_COUNT = 2
-VACANCY_COUNT = 100
-
-key_job = "v3.r.137483720.787d25efef2944db00ee9f886f29e554297a5864.572b1f3347d6d3fe8b7962d6715b0bae18bafa3d"
-
-
 class ParentApiClass(ABC):
     """Родительский класс, для классов, которые работают с API"""
+
     @abstractmethod
     def get_request(self):
         pass
@@ -27,54 +21,50 @@ class ParentApiClass(ABC):
 class HeadHunterAPI(ParentApiClass):
     """Класс для работы с API Head Hunter"""
 
-
-    def get_request(self, key_name, page):
+    def get_request(self, key_word, page):
         """Метод делает запрос на https://api.hh.ru/vacancies и возвращает результат в формате json по ключу [items]"""
         params = {
-            "text": key_name,
+            "text": key_word,
             "page": page,
             "per_page": VACANCY_COUNT,
         }
         return requests.get("https://api.hh.ru/vacancies", params=params).json()["items"]
 
-    def get_vacancies(self, key_name):
-        """"""
-        responce = []
-        for i in range(PAGE_COUNT):
-            print(f"Парсинг страницы {i + 1}", end=": ")
-            values = self.get_request(key_name, i)
-            print(f"Найдено {len(values)} вакансий")
-            responce.extend(values)
-        return responce
+    def get_vacancies(self, key_word):
+        """Функция получает список заданных вакансию по кодовому слову и возвращает его"""
+        data_list = []
+        for x in range(PAGE_COUNT):
+            values = self.get_request(key_word, x)
+            data_list.extend(values)
+        return data_list
+
 
 class SuperJobAPI(ParentApiClass):
     """Класс для взаимодейстивя с SuperJobAPI"""
     page_count = 2
-    def get_request(self, key_name, page):
 
+    def get_request(self, key_word, page):
         auth_data = {'X-Api-App-Id': key_job}
         params = {
-                "keyword": key_name,
-                "page": page,
-                "count": VACANCY_COUNT,
+            "keyword": key_word,
+            "page": page,
+            "count": VACANCY_COUNT,
         }
-        response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=auth_data, params=params).json()["objects"]
+        response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=auth_data, params=params).json()[
+            "objects"]
         return response
 
-    def get_vacancies(self, key_name):
-        """"""
-        responce = []
-        for i in range(PAGE_COUNT):
-            print(f"Парсинг страницы {i + 1}", end=": ")
-            values = self.get_request(key_name, i)
-            print(f"Найдено {len(values)} вакансий")
-            responce.extend(values)
-        return responce
+    def get_vacancies(self, key_word):
+        """Функция получает список заданных вакансию по кодовому слову и возвращает его"""
+        data_list = []
+        for x in range(PAGE_COUNT):
+            values = self.get_request(key_word, x)
+            data_list.extend(values)
+        return data_list
+
 
 class Vacancy:
     """Класс вакансий"""
-
-    #    __slots__ = ("title", "salary_from", "salary_to", "url", "employer_name")
     def __init__(self, title, salary_from, salary_to, salary_currency, url, employer_name, address):
         self.title = title
         self.url = url
@@ -97,6 +87,8 @@ class Vacancy:
             self.salary_to = f"до:{self.salary_to}"
         else:
             self.salary_to = " "
+        if not self.address:
+            self.address = "Не указано"
         msg = f"{self.employer_name}  :  {self.title}\n" \
               f"URL вакансии: {self.url} \n" \
               f"{self.salary_currency}{self.salary_from}{self.salary_to}\n" \
@@ -104,7 +96,7 @@ class Vacancy:
         return msg
 
     def __gt__(self, other):
-        """Метод сравнения"""
+        """Метод сравнения по минимальной зарплате"""
         if not other.salary_from:
             return True
         if not self.salary_from:
@@ -112,12 +104,10 @@ class Vacancy:
         return self.salary_from >= other.salary_from
 
 
-
-
 class JSONSaver:
     """Класс для работы с данными о вакансиях"""
-    def __init__(self, key_name: str, name_file: str):
-        self.__filename = f"{key_name.title()}_{name_file.lower()}.json"
+    def __init__(self, key_word: str, name_file: str):
+        self.__filename = f"{key_word.title()}_{name_file.lower()}.json"
 
     @property
     def filename(self):
@@ -129,7 +119,11 @@ class JSONSaver:
             json.dump(data, file, indent=4, ensure_ascii=False)
 
     def select(self):
-        vacansies = []
+        """Функция создает экземпляры класса Vacancy с заданными полями.
+         Если указана запралата, то пересчитываёт её в рубли(при необходимости)
+         Возвращает список вакансий
+         """
+        vacansies_list = []
         salary_from, salary_to, salary_currency = None, None, None
 
         with open(self.__filename, "r", encoding="UTF-8") as file:
@@ -141,15 +135,14 @@ class JSONSaver:
                     salary_from, salary_to, salary_currency = row["salary"]["from"], row["salary"]["to"], row["salary"][
                         "currency"]
                     if row["salary"]["currency"].upper() == "EUR":
-                        salary_from = row["salary"]["from"]*PRICE_EUR
+                        salary_from = row["salary"]["from"] * PRICE_EUR
                         salary_to = row["salary"]["to"] * PRICE_EUR
-                        # salary_currency = "RUR"
                     if row["salary"]["currency"].upper() == "USD":
-                        salary_from = row["salary"]["from"]*PRICE_USD
+                        salary_from = row["salary"]["from"] * PRICE_USD
                         salary_to = row["salary"]["to"] * PRICE_USD
                     salary_currency = "RUB"
 
-                vacansies.append(Vacancy(
+                vacansies_list.append(Vacancy(
                     row["name"],
                     salary_from,
                     salary_to,
@@ -164,15 +157,14 @@ class JSONSaver:
                     salary_from, salary_to, salary_currency = row["payment_from"], row["payment_to"], row["currency"]
 
                     if row["currency"].upper() == "EUR":
-                        salary_from = row["payment_from"]*PRICE_EUR
+                        salary_from = row["payment_from"] * PRICE_EUR
                         salary_to = row["payment_to"] * PRICE_EUR
-                        # salary_currency = "RUR"
                     if row["currency"].upper == "USD":
-                        salary_from = row["payment_from"]*PRICE_USD
+                        salary_from = row["payment_from"] * PRICE_USD
                         salary_to = row["payment_to"] * PRICE_USD
                     salary_currency = "RUB"
 
-                vacansies.append(Vacancy(
+                vacansies_list.append(Vacancy(
                     row["profession"],
                     salary_from,
                     salary_to,
@@ -181,25 +173,26 @@ class JSONSaver:
                     row["firm_name"],
                     row["address"]))
 
-        return vacansies
+        return vacansies_list
 
-if __name__ == "__main__":
-    key_name = "python developer"
-    superjob_api = SuperJobAPI()
-    sj_vacancies = superjob_api.get_vacancies(key_name)
-    json_saver = JSONSaver(key_name, "SJ")
-    json_saver.add_vacancy(sj_vacancies)
-    vacansies = json_saver.select()
-
-    sort_vacansies = get_vacancies_by_salary(vacansies, 50_000)
-    sort_vacansies = sort_from_minimum_salary(sort_vacansies, True)
-    top_vacs = get_top_vacancies(sort_vacansies, 5)
-    for i in top_vacs:
-        print(i)
-        print("\n", "="*100,  "\n")
-    print("\n\n", '*'*100, "\n\n", "the end")
-    print(len(top_vacs))
-
+#
+# if __name__ == "__main__":
+#     key_name = "python developer"
+#     superjob_api = SuperJobAPI()
+#     sj_vacancies = superjob_api.get_vacancies(key_name)
+#     json_saver = JSONSaver(key_name, "SJ")
+#     json_saver.add_vacancy(sj_vacancies)
+#     vacansies = json_saver.select()
+#
+#     sort_vacansies = get_vacancies_by_salary(vacansies, 40_000)
+#     sort_vacansies = sort_from_minimum_salary(sort_vacansies, True)
+#     top_vacs = get_top_vacancies(sort_vacansies, 5)
+#
+#     for i in top_vacs:
+#         print(i)
+#         print("\n", "=" * 100, "\n")
+#     print("\n\n", '*' * 100, "\n\n", "the end")
+#     print(len(top_vacs))
 
     # if __name__ == "__main__":
     #     key_name = "python developer"
@@ -219,10 +212,3 @@ if __name__ == "__main__":
     #         print("")
     #     print("the end")
     #     print(len(sort_vacansies))
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,51 +1,92 @@
 from classes import *
 
 
+# Создание экземпляра класса для работы с API сайтов с вакансиями
 def main():
-    """Основная функция кода"""
-    print("Введите слово, для поиска вакансии:")
-    # key_name = input()
-    key_name = "python"
-    print("привет")
+    def user_interaction():
+        print("Приветствуем, Вас")
+        search_query = input("Введите поисковый запрос: ")
+        salary_min = int(input("Укажите минимально допустимый порог зарплаты"))
+        top_n = int(input("Введите количество вакансий для вывода в топ N: "))
 
-    # Создание экземпляра класса для работы с API сайтов с вакансиями
-    hh_api = HeadHunterAPI()
-    superjob_api = SuperJobAPI()
+        # Создание экземпляра класса для работы с API сайтов с вакансиями
+        hh_api = HeadHunterAPI()
+        sj_api = SuperJobAPI()
 
-    # Получение вакансий с разных платформ
-    hh_vacancies = hh_api.get_vacancies(key_name)
-    sj_vacancies = superjob_api.get_vacancies("Python developer")
+        # Получение вакансий с разных платформ
+        hh_vacancies = hh_api.get_vacancies(search_query)
+        sj_vacancies = sj_api.get_vacancies(search_query)
 
-    # Создание экземпляра класса для работы с вакансиями
-    # vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.",
-    #                   "Требования: опыт работы от 3 лет...")
+        # Сохранение информации о вакансиях в файл
+        json_saver_hh = JSONSaver(search_query, "HH")
+        json_saver_hh.add_vacancy(hh_vacancies)
 
+        json_saver_sj = JSONSaver(search_query, "SJ")
+        json_saver_sj.add_vacancy(sj_vacancies)
+
+        # Отбор данных по вакансиям и создания списка вакансий
+        vacansies_hh = json_saver_hh.select()
+        vacansies_sj = json_saver_sj.select()
+
+        # Сортировка вакансий по минимально допустимой зарплате
+        sort_vacansies_hh = get_vacancies_by_salary(vacansies_hh, salary_min)
+        sort_vacansies_sj = get_vacancies_by_salary(vacansies_sj, salary_min)
+
+        # Сортировка списка вакансий по минимальной зарплате в убывающем порядке
+        sort_vacansies_hh = sort_from_minimum_salary(sort_vacansies_hh, True)
+        sort_vacansies_sj = sort_from_minimum_salary(sort_vacansies_sj, True)
+
+        # Отбор
+        top_vacansies_hh = get_top_vacancies(sort_vacansies_hh, top_n)
+        top_vacansies_sj = get_top_vacancies(sort_vacansies_sj, top_n)
+
+        print("\n\n", "Вывод вакансий с HeadHunter", "\n\n")
+        for vacancy in top_vacansies_hh:
+            print(vacancy)
+            print("\n", "=" * 100, "\n")
+
+        print("\n\n", "Вывод вакансий с SuperJob", "\n\n")
+        for vacancy in top_vacansies_sj:
+            print(vacancy)
+            print("\n", "=" * 100, "\n")
+
+    user_interaction()
+
+    # key_name = "python"
+
+    # hh_api = HeadHunterAPI()
+    # sj_api = SuperJobAPI()
+    #
+    # # Получение вакансий с разных платформ
+    # hh_vacancies = hh_api.get_vacancies(key_name)
+    # sj_vacancies = sj_api.get_vacancies(key_name)
+    #
+    #
     # # Сохранение информации о вакансиях в файл
-    json_saver_hh = JSONSaver(key_name, "HH")
-    json_saver_hh.add_vacancy(hh_vacancies)
-    json_saver_sj = JSONSaver(key_name, "SJ")
-    json_saver_sj.add_vacancy(sj_vacancies)
-    # json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-    # json_saver.delete_vacancy(vacancy)
+    # json_saver_hh = JSONSaver(key_name, "HH")
+    # json_saver_hh.add_vacancy(hh_vacancies)
     #
-    # # Функция для взаимодействия с пользователем
-    # def user_interaction():
-    #     platforms = ["HeadHunter", "SuperJob"]
-    #     search_query = input("Введите поисковый запрос: ")
-    #     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    #     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    #     filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
+    # json_saver_sj = JSONSaver(key_name, "SJ")
+    # json_saver_sj.add_vacancy(sj_vacancies)
     #
-    #     if not filtered_vacancies:
-    #         print("Нет вакансий, соответствующих заданным критериям.")
-    #         return
+    # vacansies_hh = json_saver_hh.select()
+    # vacansies_sj = json_saver_sj.select()
     #
-    #     sorted_vacancies = sort_vacancies(filtered_vacancies)
-    #     top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    #     print_vacancies(top_vacancies)
+    # sort_vacansies_hh = get_vacancies_by_salary(vacansies_hh, 120_000, 250_000)
+    # sort_vacansies_sj = get_vacancies_by_salary(vacansies_sj, 40_000)
     #
+    # sort_vacansies_hh = sort_from_minimum_salary(sort_vacansies_hh, True)
+    # sort_vacansies_sj = sort_from_minimum_salary(sort_vacansies_sj, True)
+    #
+    # top_vacansies_hh = get_top_vacancies(sort_vacansies_hh, 5)
+    # top_vacansies_sj = get_top_vacancies(sort_vacansies_sj, 5)
+    #
+    # for x in top_vacansies_hh:
+    #     print(x)
+    #     print("\n", "=" * 100, "\n")
+    #     print("\n\n", '*' * 100, "\n\n", "the end")
+    # print(len(top_vacansies_hh))
+
+
 if __name__ == "__main__":
     main()
-    # user_interaction()
-
-
